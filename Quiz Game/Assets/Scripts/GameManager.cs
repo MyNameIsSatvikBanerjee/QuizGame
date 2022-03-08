3782e2d29206fbc2f3d367b70823cd1312211778
MyNameIsSatvikBanerjee <66063075+MyNameIsSatvikBanerjee@users.noreply.github.com>
2022-03-08 09:11:19 +0530

    Added Game
    Game has 1 True and faslse level with some EZ questions!

---
+++ Quiz Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text factText;
+
+    [SerializeField]
+    private Text trueAnswerText;
+    [SerializeField]
+    private Text falseAnswerText;
+
+    [SerializeField]
+    private float timeBetweenQuestions = 1f;
+
+    [SerializeField]
+    private Animator animator;
+
+    void Start()
+    {
+        if (unansweredQuestions == null || unansweredQuestions.Count == 0) 
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+
+        SetCurrentQuestion();
+    }
+
+    void SetCurrentQuestion ()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        factText.text = currentQuestion.fact;
+
+        if (currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "CORRECT!";
+            falseAnswerText.text = "WRONG!";
+            falseAnswerText.color = Color.red;
+        }
+        else
+        {
+            trueAnswerText.text = "WRONG!";
+            falseAnswerText.text = "CORRECT!";
+            trueAnswerText.color = Color.red;
+        }
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+
+        yield return new WaitForSeconds(timeBetweenQuestions);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void UserSelectTrue ()
+    {
+
+        animator.SetTrigger("True");
+        
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("CORRECT!");
+        }
+        else
+        {
+            Debug.Log("WRONG!");
+
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void UserSelectFalse()
+    {
+
+        animator.SetTrigger("False");
+
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("CORRECT!");
+        }
+        else
+        {
+            Debug.Log("WRONG!");
+
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+
+    }
+}
